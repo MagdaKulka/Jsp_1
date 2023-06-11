fa6b7c698331cc30b8aab6225a3277bd3e4b20da
MagdaKulka <125749371+MagdaKulka@users.noreply.github.com>
2023-06-11 09:36:27 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const expensevalue = document.getElementById('expenensevalue');
 const incomeForm = document.getElementById('incomeForm');
 const incomesArray = [];
 const incomesList = document.getElementById('incomesList');
-const expensesArray = [];
+let expensesArray = [];
 const expensesList = document.getElementById('expensesList');
 const expenseForm = document.getElementById('expenseForm');
 
@@ -19,26 +19,60 @@ function handleIncomeFormSubmit(event) {
 	incomesArray.push({ title: event.target.incomeTitle.value, value: event.target.incomeValue.value });
 	renderIncomeList();
 	countIncomes();
+	differenceIncomesAndExpenses();
+	interator =interator +1;
 }
 function renderIncomeList() {
 	incomesList.innerHTML = '';
-	incomesArray.forEach((income) => {
+	incomesArray.forEach((income, index) => {
 		const li = document.createElement('li');
+		li.id = income.id;
+
 		li.innerHTML = `${income.title} ${income.value}`;
+		const btn = document.createElement('button');
+		btn.textContent = 'edycja';
+		li.appendChild(btn);
+		btn.addEventListener('click', () => {
+			console.log('effect');
+		});
+		const btnDelete = document.createElement('button');
+		btnDelete.textContent = 'usun';
+		li.appendChild(btnDelete);
+		btnDelete.addEventListener('click', () => {
+			console.log('effect');
+			li.remove();
+		});
 		incomesList.appendChild(li);
 	});
 }
+let iterator = 0;
 function handleExpenseFormSubmit(event) {
 	event.preventDefault();
-	expensesArray.push({ title: event.target.expenseTitle.value, value: event.target.expenseValue.value });
+	expensesArray.push({ id: iterator, title: event.target.expenseTitle.value, value: event.target.expenseValue.value });
 	renderExpenseList();
 	countExpenses();
+	differenceIncomesAndExpenses();
+	iterator = iterator + 1;
 }
 function renderExpenseList() {
 	expensesList.innerHTML = '';
-	expensesArray.forEach((expense) => {
+	expensesArray.forEach((expense, index) => {
 		const li = document.createElement('li');
+		li.id = expense.id;
 		li.innerHTML = `${expense.title} ${expense.value}`;
+		const btn = document.createElement('button');
+		btn.textContent = 'edycja';
+		li.appendChild(btn);
+		btn.addEventListener('click', () => {
+			console.log('effect');
+		});
+		const btnDelete = document.createElement('button');
+		btnDelete.textContent = 'usun';
+		li.appendChild(btnDelete);
+		btnDelete.addEventListener('click', () => {
+			calculateExpenses(expense.id);
+			li.remove();
+		});
 		expensesList.appendChild(li);
 	});
 }
@@ -48,6 +82,7 @@ function countIncomes() {
 	}, 0);
 	const incomesValue = document.getElementById('incomesValue');
 	incomesValue.innerHTML = incomesSum;
+	return incomesSum;
 }
 function countExpenses() {
 	const expensesSum = expensesArray.reduce((previousValue, currentValue) => {
@@ -55,4 +90,25 @@ function countExpenses() {
 	}, 0);
 	const expensesValue = document.getElementById('expensesValue');
 	expensesValue.innerHTML = expensesSum;
+	return expensesSum;
 }
+function differenceIncomesAndExpenses() {
+	const stateOfBudget = countIncomes() - countExpenses();
+	const difference = document.getElementById('budgetValue');
+	difference.innerHTML = stateOfBudget;
+}
+function calculateExpenses(value) {
+	expensesArray = expensesArray.filter((item) => {
+		return item.id !== value;
+	});
+	countExpenses();
+}
+function calculateIncomes(value) {
+	incomesArray = incomesArray.filter((item) => {
+		return item.id !== value;
+	});
+	countIncomes();
+stworzyc funkcję calculte incomes tam analogicznie bede przypisywała incomes Arraya tan count expenses count Income,
+jak robię hendle income zrobic lt iterator income,
+dodac id do tablicy i potem w renderincome list dodac li id i wywołanie funkcji 
+
